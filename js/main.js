17101ce1c3c2d9ada4084ac8aaca0cab23411669
Kamlesh Mehta <81505652+Kamleshmehta1@users.noreply.github.com>
2022-10-10 01:39:38 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// formData is accessible here as we have global variable in formData.js
+class Main {
+  constructor(formContainerId, formData, tableId) {
+    // start code to init and link form.js, storage.js, table.js
+    // const frm = new Form(formContainerId) // form js class to create form and access its methods
+    // const strg = new Storage(storageId)  // storage class to access storage methods
+    // const tbl = new Table(tableContainerId) // table js class to create table and access its methods
+    const form = new Form(formContainerId, formData, "mainData", tableId);
+  }
+}
+//formContainerId: HTML Div element id inside of which you want to create form4
+// formContainerId -> #employeeForm of current index.html
+
+// storageId: localStorage key for saving json  string data init
+// storageId -> 'employeeData' simple string to selected as key of localstorage
+
+//tableContainerId: HTML Div element id inside of which you want to create table
+// tableContainerId -> #tableDiv of current index.html
+const main = new Main(
+  document.querySelector("#employeeForm"),
+  formData,
+  document.querySelector("#tableDiv")
+);
